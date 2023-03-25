9bff9c55d90fe737797cdaf7ed4240a28183e256
AlexeySmirnov93 <voinvetra92@mail.ru>
2023-03-25 22:30:04 +0300

    К уроку 3

---
+++ Homework/Hom_3/ex_1.java
@@ -0,0 +1,20 @@
+package Homework.Hom_3;
+
+import java.util.ArrayList;
+
+// Пусть дан произвольный список целых чисел, удалить из него чётные числа
+
+
+public class ex_1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>(10);
+
+        for (int i = 0; i <= 10; i++) {
+            Integer rand = (int) (Math.random() * 100);
+            if (rand % 2 != 0) {
+                arrayList.add(rand);
+                System.out.println(rand);
+            }
+        }
+    }
+}
